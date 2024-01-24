33b53d4d0b1793b516460249b63e27a5e2edac36
RK11111111111 <kodream@gmail.com>
2024-01-24 17:14:53 -0500

    trapping midi CC15

--- src/mididevice.cpp
+++ src/mididevice.cpp
@@ -547,12 +547,4 @@ void CMIDIDevice::SendSystemExclusiveVoice(uint8_t nVoice, const unsigned nCable
   // Get voice sysex dump from TG
   m_pSynthesizer->getSysExVoiceDump(voicedump, nTG);
 
-  TDeviceMap::const_iterator Iterator;
-/
-  // send voice dump to all MIDI interfaces
-  for(Iterator = s_DeviceMap.begin(); Iterator != s_DeviceMap.end(); ++Iterator)
-  {
-    Iterator->second->Send (voicedump, sizeof(voicedump)*sizeof(uint8_t));
-    printf("Send SYSEX voice dump %u to \"%s\"",nVoice,Iterator->first.c_str());
-  } 
 } 

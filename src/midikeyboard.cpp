609248ba7ca17822131948d46c6a92ebd982da20
RK11111111111 <kodream@gmail.com>
2024-01-24 20:45:30 -0500

    trapping midi CC17

--- src/midikeyboard.cpp
+++ src/midikeyboard.cpp
@@ -117,10 +117,7 @@ void CMIDIKeyboard::MIDIPacketHandler0 (unsigned nCable, u8 *pPacket, unsigned n
 	u8 ucType    = ucStatus >> 4;
 
 //m_pMIDIDevice =(CUSBMIDIDevice *) CDeviceNameService::Get ()->GetDevice (m_DeviceName, FALSE);
-	printf ("MIDIDevice: cable:%s  \n",s_pThis[0]->m_DeviceName);
-		// Perform any MiniDexed level MIDI handling before specific Tone Generators
-		
-			
+
 	if(ucType==0b1011){
 		printf("Putting a message in the queue to reset MIDI_CONTROL_CHANGE@@@@@@@@");
 

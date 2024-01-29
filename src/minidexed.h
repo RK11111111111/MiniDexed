6a850d233a044418dd74fbac14708f5a2fa6099f
RK11111111111 <kodream@gmail.com>
2024-01-28 20:48:58 -0500

    Reverting mmost of the cchanges, preparing to send sysex back to alesis

--- src/minidexed.h
+++ src/minidexed.h
@@ -275,11 +275,10 @@ private:
 	CUserInterface m_UI;
 	CSysExFileLoader m_SysExFileLoader;
 	CPerformanceConfig m_PerformanceConfig;
-	char unused_buffer_bad_a[1024];
 
 	CMIDIKeyboard *m_pMIDIKeyboard[CConfig::MaxUSBMIDIDevices];
-	//CPCKeyboard m_PCKeyboard;
-	//CSerialMIDIDevice m_SerialMIDI;
+	CPCKeyboard m_PCKeyboard;
+	CSerialMIDIDevice m_SerialMIDI;
 	bool m_bUseSerial;
 
 	CSoundBaseDevice *m_pSoundDevice;
